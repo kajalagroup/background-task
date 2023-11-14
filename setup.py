12ae68696bf8cec7a36c9fd7f90ae63639b58ea3
Tuan Bach Van <bachvtuan@gmail.com>
2023-11-14 17:35:17 +0700

    Correct description type

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="django-kcommunication",
-    version="1.1.7",
+    version="1.1.8",
     author=u"Tuan Bach Van",
     author_email="tuan@kajala.com",
     packages=find_packages(exclude=["project", "venv"]),
@@ -22,6 +22,7 @@ setup(
     license="MIT licence, see LICENCE.txt",
     description="Communication helpers and background task management for Huey queue.",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     zip_safe=False,
     install_requires=install_requires,
 )
